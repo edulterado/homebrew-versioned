1eb2bb2d4a6f322eb23d28682534f579a064af3c
Eduardo Martínez <edulterado@gmail.com>
2020-09-26 21:42:48 +0200

    Add ocaml 4.07.0 Formula

--- Formula/camlp4@4.08+1.rb
+++ Formula/camlp4@4.08+1.rb
@@ -6,14 +6,6 @@ class Camlp4AT408x1 < Formula
   sha256 "655cd3bdcafbf8435877f60f4b47dd2eb69feef5afd8881291ef01ba12bd9d88"
   head "https://github.com/ocaml/camlp4.git", :branch => "trunk"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "1f1cc081a2a79b1d41ac7f109800a730ad6ffb70b4ba1f83a72619786093a37a" => :catalina
-    sha256 "0eeb91e592213855883d9387bbcb2a034250df388f5f3499210cc5aed60267c8" => :mojave
-    sha256 "9fa35bb3cefc703afab585f87687df22a3542a7c076966693c5c8a663a3220d3" => :high_sierra
-    sha256 "e39318a0bc39b66821eb35ac0a706f61754d2c27355bfe7da5a68a7bff05243c" => :sierra
-  end
-
   depends_on "ocaml"
   # since OCaml 4.03.0, ocamlbuild is no longer part of ocaml
   depends_on "ocamlbuild"
